05957c5a17d24d8ab2a9349595bf9a852243e5e8
Priti-i <pritiy049@gmail.com>
2025-08-20 13:16:08 +0530

    string and no. combination

---
+++ someNew.js
@@ -0,0 +1,8 @@
+let a=10;
+    let b=20;
+ console.log(a+b) // it returns 30 as both are numbers and it performs addition
+console.log(a+b + " is the sum of a and b")
+ console.log( "addition of a and b is"+a+b); // it returns "addition of a and b is1020" as it concatenates the string with the number
+console.log ("1"+1); // it return 11 here not addition its concatination
+console.log("1"-10); // it return -9 here because it converts "1" to number and then subtracts
+
