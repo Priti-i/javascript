89ff8780cc25eda2346207ecf60eda5172a36432
Priti-i <pritiy049@gmail.com>
2025-08-21 22:55:45 +0530

    math function

--- Mathfun.js
+++ Mathfun.js
@@ -6,3 +6,12 @@ console.log(Math.floor(-4.9)); // -5
 Math.ceil(x)  // Rounds up to the nearest integer.
 console.log(Math.ceil(4.1)); // 5
 console.log(Math.ceil(-4.1)); // -4
+
+Math.round(x) //  Rounds to the nearest integer (0.5 and above rounds up).
+console.log(Math.round(4.4)); // 4
+console.log(Math.round(4.6)); // 5
+
+Math.trunc(x)  // Removes the decimal part (just keeps the integer).
+
+console.log(Math.trunc(4.9));  // 4
+console.log(Math.trunc(-4.9)); // -4
