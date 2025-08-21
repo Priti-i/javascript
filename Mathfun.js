bc0a908044cf62268fb3a83e87b5425e0015e921
Priti-i <pritiy049@gmail.com>
2025-08-21 22:53:05 +0530

    math function

---
+++ Mathfun.js
@@ -0,0 +1,8 @@
+Math.floor(x) // Rounds a number down to the nearest integer.
+console.log(Math.floor(4.9)); // 4
+console.log(Math.floor(-4.9)); // -5
+
+
+Math.ceil(x)  // Rounds up to the nearest integer.
+console.log(Math.ceil(4.1)); // 5
+console.log(Math.ceil(-4.1)); // -4
--- someNew.js
+++ someNew.js
@@ -1,5 +1,5 @@
 let a=10;
-    let b=20;
+let b=20;
  console.log(a+b) // it returns 30 as both are numbers and it performs addition
 console.log(a+b + " is the sum of a and b")
  console.log( "addition of a and b is"+a+b); // it returns "addition of a and b is1020" as it concatenates the string with the number
