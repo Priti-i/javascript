ea6064ce9d2866310e259b85014ab01c6653ffa4
Priti-i <pritiy049@gmail.com>
2025-09-09 22:16:56 +0530

    string

--- Array/ReverseArr.js
+++ Array/ReverseArr.js
@@ -1,4 +1,4 @@
-// reverse aray
+// reverse array
 
 function Reverse(arr){
     for(let i=arr.length;i>=0;i--)
@@ -6,4 +6,4 @@ function Reverse(arr){
         console.log(arr[i]);
     }
 }
-console.log(Reverse([1,2,3,4,5,6,7,8,9]))
+console.log(Reverse([1,2,3,4,5,6,7,8,9]));
