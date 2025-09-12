8ec7d69daa761dd19a5a6a98aacd2f45d5232db3
Priti-i <pritiy049@gmail.com>
2025-09-12 12:44:49 +0530

    revrese strng and every character in new line

---
+++ String/ReverseString.js
@@ -0,0 +1,7 @@
+let str="pritiyadav";
+let reverseStr="";
+for(let i=str.length-1;i>=0;i--)
+{
+    reverseStr+=str.charAt(i);
+}
+console.log(reverseStr);
---
+++ String/newlineCharacrter.js
@@ -0,0 +1,7 @@
+let str1="hello";
+let str2="world";
+
+for(let i=str1.length-1;i>=0;i--)
+{
+    console.log(str1.charAt(i));
+}
