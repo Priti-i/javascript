37148631122bd0b1bd56fb5245776c7e6d9a5a49
Priti-i <pritiy049@gmail.com>
2025-09-19 15:16:53 +0530

    insertion sort

--- Sorting/Selection.js
+++ Sorting/Selection.js
@@ -1,22 +1,16 @@
-const SelectionSort=(arr)=>{
-    let n=arr.length;
-    for(let i=0;i<n;i++)
-        {
-        let minIndex=i;
-        for(let j=i+1;j<n;j++){
-            if(arr[j]<arr[minIndex])
-            
-                minIndex=j;
-        }
-             if(minIndex!=i)
-{
-    let temp=arr[i];
-    arr[i]=arr[minIndex];
-    arr[minIndex]=temp;
-}
-           
-          }
-          return arr;
-
-}
-console.log(SelectionSort([64,34,25,12,22,11,90]));
+const SelectionSort = (arr) => {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) minIndex = j;
+    }
+    if (minIndex != i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp; 
+    }
+  }
+  return arr;
+};
+console.log(SelectionSort([64, 34, 25, 12, 22, 11, 90]));
