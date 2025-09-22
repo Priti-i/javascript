3542b0627307be34f5f90d7bc7d43205bb8ac968
Priti-i <pritiy049@gmail.com>
2025-09-22 22:11:40 +0530

    recursion

---
+++ Recursion.js
@@ -0,0 +1,5 @@
+/*What is Recursion?
+
+Recursion is a programming technique where a function calls itself directly or indirectly to solve a problem.
+It usually works by breaking a big problem into smaller sub-problems of the same type.
+*/
